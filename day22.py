bbeba119bd9443a376c2aade6e132083c07ed89f
nhannht <nhanclassroom@gmail.com>
2022-12-23 13:55:49 +0700

    part 1, finsih small sample part 1, have bug in real input

--- day22.py
+++ day22.py
@@ -31,8 +31,7 @@ def move_one_step():
                     break
             # if next pos is a walk, return to the original pos
             if field[row_next][col_next] == '#':
-                current_direction = "left"
-                row_next,col_next = move_one_step()
+                return current_pos[0], current_pos[1]
         if field[row_next][col_next] == ' ':
             while True:
                 col_next -= 1
@@ -40,17 +39,14 @@ def move_one_step():
                     col_next += 1
                     break
             if field[row_next][col_next] == '#':
-                current_direction = "left"
-                row_next, col_next = move_one_step()
+                return current_pos[0], current_pos[1]
 
         if field[row_next][col_next] == '#':
-            current_direction = "left"
-            row_next, col_next = move_one_step()
+            return current_pos[0], current_pos[1]
 
     elif current_direction == "left":
         col_next = current_pos[1] - 1
         row_next = current_pos[0]
-        # if next pos is outsize the field, wrap around other size
         if col_next < 0:
             while True:
                 col_next += 1
@@ -58,8 +54,7 @@ def move_one_step():
                     col_next -= 1
                     break
             if field[row_next][col_next] == '#':
-                current_direction = "right"
-                row_next, col_next = move_one_step()
+                return current_pos[0], current_pos[1]
         if field[row_next][col_next] == ' ':
             while True:
                 col_next += 1
@@ -67,17 +62,14 @@ def move_one_step():
                     col_next -= 1
                     break
             if field[row_next][col_next] == '#':
-                current_direction = "right"
-                row_next,col_next = move_one_step()
+                return current_pos[0], current_pos[1]
 
         if field[row_next][col_next] == '#':
-            current_direction = "right"
-            row_next, col_next = move_one_step()
+            return current_pos[0], current_pos[1]
 
     elif current_direction == "up":
         col_next = current_pos[1]
         row_next = current_pos[0] - 1
-        # if next pos is outsize the field, wrap around other size
         if row_next < 0:
             while True:
                 row_next += 1
@@ -85,8 +77,7 @@ def move_one_step():
                     row_next -= 1
                     break
             if field[row_next][col_next] == '#':
-                current_direction = "down"
-                row_next, col_next = move_one_step()
+                return current_pos[0], current_pos[1]
         if field[row_next][col_next] == ' ':
             while True:
                 row_next += 1
@@ -94,17 +85,14 @@ def move_one_step():
                     row_next -= 1
                     break
             if field[row_next][col_next] == '#':
-                current_direction = "down"
-                row_next, col_next = move_one_step()
+                return current_pos[0], current_pos[1]
 
         if field[row_next][col_next] == '#':
-            current_direction = "down"
-            row_next,col_next = move_one_step()
+            return current_pos[0], current_pos[1]
 
     elif current_direction == "down":
         col_next = current_pos[1]
         row_next = current_pos[0] + 1
-        # if next pos is outsize the field, wrap around other size
         if row_next >= len(field):
             while True:
                 row_next -= 1
@@ -112,8 +100,7 @@ def move_one_step():
                     row_next += 1
                     break
             if field[row_next][col_next] == '#':
-                current_direction = "up"
-                row_next,col_next = move_one_step()
+                return current_pos[0], current_pos[1]
         if field[row_next][col_next] == ' ':
             while True:
                 row_next -= 1
@@ -121,36 +108,33 @@ def move_one_step():
                     row_next += 1
                     break
             if field[row_next][col_next] == '#':
-                current_direction = "up"
-                row_next, col_next = move_one_step()
+                return current_pos[0], current_pos[1]
 
         if field[row_next][col_next] == '#':
-            current_direction = "up"
-            row_next, col_next = move_one_step()
-
+            return current_pos[0], current_pos[1]
 
 
     return row_next, col_next
 
 
 # %%
+from copy import deepcopy
 current_pos = [0, field[0].index('.')]
+field_clone = deepcopy(field)
 current_direction = "right"
 for instruct in instructions:
-
-    print(f"current pos: {current_pos}")
+    # print(f"current pos: {current_pos}")
     if instruct.isdigit():
         for i in range(int(instruct)):
             current_pos = move_one_step()
             if current_direction == "right":
-                field[current_pos[0]][current_pos[1]] = '>'
+                field_clone[current_pos[0]][current_pos[1]] = '>'
             elif current_direction == "left":
-                field[current_pos[0]][current_pos[1]] = '<'
+                field_clone[current_pos[0]][current_pos[1]] = '<'
             elif current_direction == "up":
-                field[current_pos[0]][current_pos[1]] = '^'
+                field_clone[current_pos[0]][current_pos[1]] = '^'
             elif current_direction == "down":
-                field[current_pos[0]][current_pos[1]] = 'v'
-
+                field_clone[current_pos[0]][current_pos[1]] = 'v'
     if instruct == 'R':
         if current_direction == "right":
             current_direction = "down"
@@ -171,12 +155,20 @@ for instruct in instructions:
             current_direction = "right"
 
 # mark last position
-field[current_pos[0]][current_pos[1]] = 'X'
-# %%
-for row in field:
+field_clone[current_pos[0]][current_pos[1]] = 'X'
+print(f"current pos: {current_pos}")
+print(f"current direction: {current_direction}")
+result_of_part_1 = 0
+if current_direction == "right":
+    result_of_part_1 = (current_pos[0]+ 1) * 1000 + 4 *( current_pos[1]+1) + 0
+elif current_direction == "down":
+    result_of_part_1 = (current_pos[0]+ 1) * 1000 +4 * ( current_pos[1]+1) + 1
+elif current_direction == "left":
+    result_of_part_1 = (current_pos[0]+ 1) * 1000 + 4*( current_pos[1]+1) + 2
+elif current_direction == "up":
+    result_of_part_1 = (current_pos[0]+ 1) * 1000 +4*( current_pos[1]+1) + 3
+#%%
+for row in field_clone:
     print(''.join(row))
 # %%
-current_pos = [0, field[0].index('.')]
-current_direction = "right"
 # %%
-current_pos = move_one_step()
